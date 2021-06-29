161175f9c606cbb896372b663b303f87af2912f1
venashial <venashial.levo@aleeas.com>
2021-06-29 15:40:31 -0700

    Updated ENV detection

--- src/database/knex.js
+++ src/database/knex.js
@@ -1,3 +1,3 @@
-const environment = process.env.ENVIRONMENT || 'development'
+const environment = process.env.NODE_ENV
 const config = require('../../knexfile.js')[environment];
 module.exports = require('knex')(config);
